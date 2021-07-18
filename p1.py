dbaecdb264059778bb246ecc3e1459e17c4178d1
SwapnilMahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2021-07-18 23:37:09 +0600

    pe
    pe

--- p1.py
+++
@@ -1,9 +0,0 @@
-row=5
-for i in range(0,row):
-    for j in range(0,i+1):
-        print('*',end=' ')
-    print('\r')
-for i in range(row,0,-1):
-    for j in range(0,i-1):
-        print('*',end=' ')
-    print('\r')
