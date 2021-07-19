14c8b2c3fbe400d8a8120e2533a68e67fe2c8071
SwapnilMahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2021-07-19 11:42:24 +0600

    function
    f

--- function.py
+++ function.py
@@ -1,6 +1,20 @@
-bst=[ ]
-n=int(input("please enter the array size:"))
-for i in range(0,n):
-    e=int(input())
-    bst.append(e)
-print(bst)
+def ar():  
+    bst=[ ]
+    t=int(input("target:"))
+    n=int(input("size:"))
+    for i in range(0,n):
+        e=int(input())
+        bst.append(e)
+        print("size:",bst)
+    left=0
+    right=len(bst)-1
+    curr=bst[left]+bst[right]
+    while left<=right:
+        if curr<t:
+            left+=1
+        elif curr>t:
+            right-=1
+        else:
+            return[left,right]
+    return[-1,-1]    
+ar()
