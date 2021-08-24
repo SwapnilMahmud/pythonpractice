7b7eee405da3d714d2b701fc29723cdaf0d18ec6
SwapnilMahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2021-08-24 19:38:27 +0600

    sd
    d

--- problem solving.py
+++ problem solving.py
@@ -779,3 +779,23 @@ for i in range(n):
             cnt+=1
             print(cnt)
 print(cnt)
+::::::::::::::::::::::::::::binary search python:::::::::;;
+def BinarySearch(arr,n,data):
+    l=0
+    r=n-1
+    while l<=r:
+        m=(l+r)/2
+        mid=int(m)
+        if data==arr[mid]:
+            return mid
+        elif data<arr[mid]:
+            r=mid-1
+        else:
+            l=mid+1
+    return -1
+
+arr=[10,22,34,55,57,67,70,88,99]
+data=99
+l=len(arr)
+print(BinarySearch(arr,l,data))
+
