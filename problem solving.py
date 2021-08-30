35036baaf53a2d1c31cd19223d0488c3b11c7709
SwapnilMahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2021-08-30 10:31:40 +0600

    Update problem solving.py

--- problem solving.py
+++ problem solving.py
@@ -841,4 +841,13 @@ for i in range(n):
         break
 if found==0:
     print("data not found")
-        
+     :::::::::::::::::::::::::::::::::::::::::::::::cf136 presents
+n=int(input())
+li=list(map(int,input().split()))
+p=[0]*n
+for i in range(n):
+    p[li[i]-1]=i+1
+for i in range(n):
+    print(p[i],end=" ")
+    
+
