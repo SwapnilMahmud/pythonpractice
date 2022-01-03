4f2c268b32ddec912375539d9cb29b9e4a4579e6
Mahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2022-01-03 11:11:55 +0600

    l
    ,

---
+++ basic.py
@@ -0,0 +1,80 @@
+li=[]
+li.append(3)
+li.append(4)
+li.append(5)
+li.append(6)
+li.append(7)
+li.append(8)
+li.append(9)
+li.append(10)
+li.append(11)
+print(li)
+li.pop()
+li.pop()
+print(li)
+print(len(li))
+print(li[2])
+print(li[3])
+print("..............................................for searching")
+
+for element in li:
+    print(element)
+print("..............................................")
+
+
+
+li=[]
+li.append(3)
+li.append(4)
+li.append(5)
+li.append(6)
+li.append(7)
+li.append(8)
+li.append(9)
+li.append(10)
+li.append(11)
+print(li)
+
+target=8
+for element in li:
+    if target==element:
+        print("found")
+    else:
+        print("not found")
+        
+		
+		
+start=len(li)-1
+while start>=0:
+    print(li[start])
+    start-=1
+    
+    
+    
+    
+    
+    
+    
+  ...........................................
+  li=[]
+li.append(3)
+li.append(4)
+li.append(5)
+li.append(9)
+li.append(10)
+li.append(11)
+print(li)
+s=0
+e=len(li)-1
+while s<e:
+    temp=li[s]
+    li[s]=li[e]
+    li[e]=temp
+    s+=1
+    e-=1
+print(li)
+
+
+
+
+    
